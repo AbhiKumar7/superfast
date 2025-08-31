cc982786ab7ca5d11eb54db5ceb83b0d03dcb6de
user.name "Abhishek kumar <user.email "abkumar654gmail.com>
2025-08-31 12:35:43 +0530

    Fix case-sensitive import issue for customToast

--- frontend/superfast/src/utils/CustomToast.jsx
+++ frontend/superfast/src/utils/CustomToast.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { useEffect } from "react";
+// import React from "react";
+// import { useEffect } from "react";
 
-function CustomToast({ message, onClose }) {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      onClose(); 
-    }, 3000);
+// function CustomToast({ message, onClose }) {
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       onClose(); 
+//     }, 3000);
 
-    return () => clearTimeout(timer);
-  }, []);
-  return (
-    <div className="fixed top-5 right-5 bg-green-500 text-white px-4 py-2 rounded shadow-lg transition-all z-50">
-      {message}
-    </div>
-  );
-}
+//     return () => clearTimeout(timer);
+//   }, []);
+//   return (
+//     <div className="fixed top-5 right-5 bg-green-500 text-white px-4 py-2 rounded shadow-lg transition-all z-50">
+//       {message}
+//     </div>
+//   );
+// }
 
-export default CustomToast;
+// export default CustomToast;
